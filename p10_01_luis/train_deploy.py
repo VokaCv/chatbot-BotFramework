06f03cab9718e5690f1bce890544776609a20619
Marko Cvokic <voka.c@hotmail.com>
2022-02-25 08:37:59 +0100

    another test

--- p10_01_luis/train_deploy.py
+++ p10_01_luis/train_deploy.py
@@ -45,63 +45,61 @@ if __name__ == "__main__":
     
     print("Chargement des variables d'environement.")   
     env = LuisEnv()
-    print('LUIS ENV APP ID', env.LUIS_APP_ID)
     
-    
+   
     print("Chargement du workspace.")
     # On charge l’espace de travail Azure ML
     ws = azure_helper.get_ws()
-    print('WPORKSPAVCE', ws)
     
-    # print("Chargement des paramètres du modèle.")
-    # latest_version = get_latest_version(env)
-    # params = get_params(env, latest_version)
+    print("Chargement des paramètres du modèle.")
+    latest_version = get_latest_version(env)
+    params = get_params(env, latest_version)
         
-    # # model_version = str(params["model"]["versionId"])
-    # # ds_name = params["dataset"]["name"]
-    # # ds_version = params["dataset"]["version"]
+    # model_version = str(params["model"]["versionId"])
+    # ds_name = params["dataset"]["name"]
+    # ds_version = params["dataset"]["version"]
         
-    # print("Chargement des jeux de données.")
+    print("Chargement des jeux de données.")
     
-    # dataset = Dataset.get_by_name(ws, 'utterances')
-    # dataset.download(target_path='.', overwrite=True)
+    dataset = Dataset.get_by_name(ws, 'utterances')
+    dataset.download(target_path='.', overwrite=True)
         
-    # # On charge le jeu d'entrainement
-    # file_path = os.path.join('.', "utterances_train.json")
-    # with open(file_path) as f:
-    #     utterances_train = json.load(f)
+    # On charge le jeu d'entrainement
+    file_path = os.path.join('.', "utterances_train.json")
+    with open(file_path) as f:
+        utterances_train = json.load(f)
     
-    # file_path1 = os.path.join('.', "utterances_test.json")
-    # with open(file_path1) as f:
-    #     utterances_test= json.load(f)
+    file_path1 = os.path.join('.', "utterances_test.json")
+    with open(file_path1) as f:
+        utterances_test= json.load(f)
 
-    # os.remove(file_path)
-    # os.remove(file_path1)
+    os.remove(file_path)
+    os.remove(file_path1)
     
-    # # # On crée le nom de version du modèle
-    # app_version = round(float(latest_version+0.1),2)    
-    # print(f"Création de la version {app_version}.")
+    # # On crée le nom de version du modèle
+    app_version = round(float(latest_version+0.1),2)    
+    print(f"Création de la version {app_version}.")
    
-    # create_new_version(env, app_version, params, utterances_train)
+    create_new_version(env, app_version, params, utterances_train)
     
-    # print(f"Entrainement du modèle...")    
-    # train_luis(env, app_version)
+    print(f"Entrainement du modèle...")    
+    train_luis(env, app_version)
     
-    # print(f"Déploiement du modèle...")    
+    print(f"Déploiement du modèle...")    
+    if args.is_staging == 1:
+        slots = "staging" 
+    else:
+        slots = "production"
+
+    deploy_luis(env, app_version, is_staging=slots)
+    
+
+    print(f"Evaluation du modèle...")
+    res = evaluate(env, True, utterances_test)
+    # On affiche les résultats
+    print(res.to_markdown())
+    
+    # # On supprime le modèle si besoin
     # if args.is_staging == 1:
-    #     slots = "staging" 
-    # else:
-    #     slots = "production"
-
-    # deploy_luis(env, app_version, is_staging=slots)
-    
-
-    # print(f"Evaluation du modèle...")
-    # res = evaluate(env, True, utterances_test)
-    # # On affiche les résultats
-    # print(res.to_markdown())
-    
-    # # # On supprime le modèle si besoin
-    # # if args.is_staging == 1:
-    # #     print("Suppression du modèle.")
-    # #     delete_luis(env, app_version)
+    #     print("Suppression du modèle.")
+    #     delete_luis(env, app_version)
